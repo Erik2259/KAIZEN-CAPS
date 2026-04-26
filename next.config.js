981a10fd4d80ae5daf41e0f7b89292cee5364db1
ERIK H <hernandezaparicioerik3@gmail.com>
2026-04-26 11:01:35 -0600

    Add image formats and minimum cache TTL to config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: '*.supabase.co', pathname: '/storage/v1/object/public/**' },
     ],
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 60 * 60 * 24 * 30,
   },
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true },
